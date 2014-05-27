3b7eed28437f9f507302b8f5d7d3ebd50cff7260
amy801101 <amy19911101@gmail.com>
2014-05-27 22:07:37 +0800

    Complete adding account for sign_up in regestrations view

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,4 +3,6 @@ class Member < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates_presence_of :account, uniqueness: true
 end
